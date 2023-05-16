b15ded66cfbef4e8d6ea99fcb123d3932644dbae
lucas <mr.pine97@gmail.com>
2023-05-16 18:22:56 -0300

    adiciona logica para atualizar um produto

--- src/app/product-detail/product-detail.page.ts
+++ src/app/product-detail/product-detail.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ProductService } from '../services/product.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-product-detail',
@@ -11,9 +12,17 @@ export class ProductDetailPage implements OnInit {
   isLoading = true;
   productFound = false;
   product: any;
+  productForm: FormGroup = this.fb.group({
+    name: ['', Validators.required],
+    description: ['', Validators.required],
+    price: ['', Validators.required]
+  });
+  isUpdating = false;
 
   constructor(private route: ActivatedRoute,
-    private productService: ProductService) { }
+    private router: Router,
+    private productService: ProductService,
+    public fb: FormBuilder) { }
 
   getProductDetails(productId: string): void {
     this.productService.getProduct(productId).subscribe(product => {
@@ -24,6 +33,18 @@ export class ProductDetailPage implements OnInit {
         this.productFound = true;
       }
     });
+  }
+
+  updateProduct() {
+    const productId = this.route.snapshot.params['id'];
+    this.productService.updateProduct(productId, this.productForm.value)
+      .then(() => {
+        this.isUpdating = false;
+        this.router.navigateByUrl(`/products/${productId}`);
+      })
+      .catch(error => {
+        console.error('Error updating product:', error);
+      });
   }
 
   ngOnInit() {
