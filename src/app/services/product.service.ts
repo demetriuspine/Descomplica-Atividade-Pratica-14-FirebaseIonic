baabe2d5ba2d020473aaf94dcff0dbf62548dc7e
lucas <mr.pine97@gmail.com>
2023-05-16 16:06:18 -0300

    refatora getProduct e getProductList

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase, AngularFireList, AngularFireObject } from '@angular/fire/compat/database';
 import { IProduct } from '../models/product.interface';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -24,15 +25,15 @@ export class ProductService {
   }
 
   // Get Single
-  getProduct(id: string) {
+  getProduct(id: string): Observable<IProduct | null> {
     const productRef: AngularFireObject<IProduct> = this.db.object('/products/' + id);
-    return productRef;
+    return productRef.valueChanges();
   }
 
   // Get List
   getProductList() {
-    this.productListRef = this.db.list('/products');
-    return this.productListRef;
+    this.productListRef = this.db.list<IProduct>('/products');
+    return this.productListRef.snapshotChanges();
   }
 
   // Update
